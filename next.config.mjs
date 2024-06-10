5647e165d0db4ed2e5243e14fa3385eb32c68973
fikrialwan <fikriar93@gmail.com>
2024-06-10 21:26:34 +0700

    chore: add next pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+/** @type {import("next").NextConfig} */
+const config = {};
+
+const withPWA = withPWAInit({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+});
+
+export default withPWA(config);
